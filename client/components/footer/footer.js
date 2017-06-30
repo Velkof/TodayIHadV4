99917de84afad2a57256022ceaafb9b1d47dc0ca
Marjan <velkof@outlook.com>
2017-06-30 17:55:33 +0200

    UI changes on footer

--- client/components/footer/footer.js
+++ client/components/footer/footer.js
@@ -10,15 +10,21 @@ class Footer extends Component {
     render() {
       return (
         <div className="footer">
-            <div className="container-mob">
-                <div className="col-xs-3">
-                    <Link to={'/dashboard'} ><span className="glyphicon glyphicon-home" style={{fontSize:'2rem'}}></span></Link> <br/><br/>
+            <div className="container-mob px-0">
+                <div className="col-xs-5ths">
+                    <Link to={'/dashboard'} ><span className="glyphicon glyphicon-home footerTab"></span></Link> <br/><br/>
                 </div>
-                <div className="col-xs-3">
-                    <Link to={'/foods'} ><span className="glyphicon glyphicon-apple" style={{fontSize:'2rem'}}></span></Link><br/><br/>
+                <div className="col-xs-5ths">
+                    <Link to={'/foods/add'} ><span className="glyphicon glyphicon-search footerTab" ></span></Link><br/><br/>
                 </div>
-                <div className="col-xs-3">
-                    <Link to={'/foods/add'} ><span className="glyphicon glyphicon-plus" style={{fontSize:'2rem'}}></span></Link><br/><br/>
+                <div className="col-xs-5ths">
+                    <Link to={'/foods/add'} ><span className="glyphicon glyphicon-plus footerTab" ></span></Link><br/><br/>
+                </div>
+                <div className="col-xs-5ths">
+                    <Link to={'/foods'} ><span className="glyphicon glyphicon-apple footerTab"></span></Link><br/><br/>
+                </div>
+                <div className="col-xs-5ths">
+                    <Link to={'/foods/add'} ><span className="glyphicon glyphicon-bell footerTab" ></span></Link><br/><br/>
                 </div>
             </div>
         </div>
